746aa5aec3cf6fef905b31b08224e2ab4624eb6e
Hugo Delattre <delattre.hugo.24@gmail.com>
2024-04-06 21:09:41 +0200

    feat: navbar responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,48 @@
-import Button from "@/components/Button";
-import { navigation } from "@/constants";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import MenuSvg from "@/../public/assets/svg/MenuSvg";
+import Button from "@/components/Button";
+import { navigation } from "@/constants";
+import { HamburgerMenu } from "@/components/design/Header";
 
 const Header = () => {
   const { pathname } = useRouter();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleClick = () => {
+    if(!openNavigation) return;
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backgrop-blur-small"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block  w-[12rem] xl:mr-8" href="#hero">
           Logo
         </a>
-        <nav className="hidden fixed top-[5rem] left-0 right-0  bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg-bg-transparent">
+        <nav
+          className={` ${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0  bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg-bg-transparent`}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <Link
@@ -23,11 +53,13 @@ const Header = () => {
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
                 key={item.id}
                 href={item.url}
+                onClick={handleClick}
               >
                 {item.title}
               </Link>
             ))}
           </div>
+          <HamburgerMenu />
         </nav>
         <a
           href="#signup"
@@ -37,6 +69,13 @@ const Header = () => {
         </a>
         <Button className="hidden lg:flex" href="#login">
           Hey
+        </Button>
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
